b0b8a46cdad61c86e27bd2980113ca68a59e669a
MinhQuan <minhquan23102000@gmail.com>
2021-04-20 09:50:22 +0700

    Đã thêm xuất các account. Và đăng ký route với url là :lol-account + login

--- ShopAccGame/App_Start/RouteConfig.cs
+++ ShopAccGame/App_Start/RouteConfig.cs
@@ -14,10 +14,26 @@ namespace ShopAccGame
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+               name: "Product",
+               url: "lol-account",
+               defaults: new { controller = "Product", action = "Index", id = UrlParameter.Optional }
+           );
+
+            routes.MapRoute(
+               name: "Login",
+               url: "login",
+               defaults: new { controller = "Login", action = "Index", id = UrlParameter.Optional }
+           );
+
+            // Nhớ đăng ký map route ở trên này, ở dưới đây thì nó không nhận
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+
+           
+
         }
     }
 }
