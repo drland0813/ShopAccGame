4335d90f580897e65dd1364e13dea2a4a633d84a
MinhQuan <minhquan23102000@gmail.com>
2021-04-20 10:18:36 +0700

    Thêm Controller với View

---
+++ ShopAccGame/Controllers/ProductController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ShopAccGame.Models.DataRequester;
+using ShopAccGame.Models.MyData;
+namespace ShopAccGame.Controllers
+{
+    public class ProductController : Controller
+    {
+
+        public static readonly string[] ranks = new string[]
+        {
+            "Iron",
+            "Copper",
+            "Silver",
+            "Gold",
+            "Platinum",
+            "Diamond",
+            "Master",
+            "Grand Master",
+            "Challenger",
+            "Rankless"
+        };
+
+        // GET: Product
+        public ActionResult Index()
+        {
+
+            AccountDAO accountDAO = new AccountDAO();
+            ICollection<Account> lolAccounts = accountDAO.getListAccounts();
+            ViewData["lolAccounts"] = lolAccounts;
+            ViewData["ranks"] = ranks;
+            return View();
+        }
+    }
+}
